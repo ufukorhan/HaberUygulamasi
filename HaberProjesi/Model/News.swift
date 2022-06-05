891fc5062c0b6d845151a7a2c509ce9bb54768fb
Ufuk ORHAN <ufuk.orhan@logsign.net>
2022-06-05 13:38:05 +0300

    feat: add news app

---
+++ HaberProjesi/Model/News.swift
@@ -0,0 +1,13 @@
+//
+//  News.swift
+//  HaberProjesi
+//
+//  Created by Ufuk Orhan on 04.06.2022.
+//
+
+import Foundation
+
+struct News : Decodable {
+    let title: String
+    let story: String
+}
